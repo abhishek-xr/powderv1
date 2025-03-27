234419954bc69ea8158902fb96759dbfa72eeef9
abhishek-xr <ramchandani.a@northeastern.edu>
2025-03-27 18:53:57 -0400

    update

--- backend/database.py
+++ backend/database.py
@@ -5,8 +5,11 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:7828@localhost/portfolio-db")
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 
 engine = create_engine(DATABASE_URL)
